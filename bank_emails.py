e4fd3e46f8bc5bb3fcd27c6fe9b69a92e20175a3
arielfayol37 <115123676+arielfayol37@users.noreply.github.com>
2022-12-29 19:22:59 -0500

    Update bank_emails.py

--- bank_emails.py
+++ bank_emails.py
@@ -1,8 +1,8 @@
 
-import re, openpyxl
-from bank_email_formats import find_bank_extension
+import re, openpyxl, os
+from bank_email_formats import find_bank_domain
     
-
+##Generating possible local part of people's emails using their first and last names
 def gen_emails(people_string, extension, format ='f1'):
     people_list = people_string.split("; ")
     error = 0
@@ -36,28 +36,30 @@ def gen_emails(people_string, extension, format ='f1'):
                 emails.append(email)                
         except:
             error+=1
-    #print(emails)
+    
     return (names, emails, error)
 
-filename = 'bank_names.xlsx'
+filename = 'bank_names.xlsx' #Excel file containing a list of banks, their capital, and executive members
 wb = openpyxl.load_workbook(filename)
-sheet = wb['Screening']
+sheet = wb['Screening'] #this is the sheet with the information we need
 new_wb = openpyxl.Workbook()
-filename2 = 'email_of_bankers.xlsx'
+filename2 = 'emails_of_bankers.xlsx' #excel file where we will save our newly generated emails
 sheet2 = new_wb['Sheet']
-num_cells = 6065
+num_cells = 6065 
 cell_2_num = 2
 total_errors1 = 0
 total_errors2 = 0
 counter = 0
 for i in range(9, num_cells):
     counter += 1
-    bank_name = sheet['A' + str(i)].value 
-    extension = find_bank_extension(bank_name)
+    print(counter)
+    bank_capital = sheet['A' + str(i)].value
+    capital = sheet['D' + str(i)].value 
+    extension,search_url = find_bank_domain(bank_name)
     if extension is not None:
         people_names = sheet["I" + str(i)].value 
-        names1, emails1, error1 = gen_emails(people_names,  f'{extension}.com', 'f1')
-        names2,emails2, error2 = gen_emails(people_names,  f'{extension}.com', 'f2')
+        names1, emails1, error1 = gen_emails(people_names,  f'{extension}', 'f1')
+        names2,emails2, error2 = gen_emails(people_names,  f'{extension}', 'f2')
         total_errors1 += error1
         total_errors2 += error2
         for name,email1,email2 in zip(names1,emails1, emails2):
@@ -65,13 +67,16 @@ for i in range(9, num_cells):
             sheet2["B"+str(cell_2_num)].value = email1
             sheet2["C"+str(cell_2_num)].value = email2
             sheet2['D'+str(cell_2_num)].value = bank_name
+            sheet2['E'+str(cell_2_num)].value = bank_capital
+            sheet2['F'+str(cell_2_num)].value = search_url
+            
             cell_2_num += 1
-        new_wb.save(filename2)
+        
     elif extension == -1:
         print(counter)
         
         break
-
+    new_wb.save(filename2)
 print(total_errors1)
 print(total_errors2)
 
